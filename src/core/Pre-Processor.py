7e49142e5d7e069013274b65bfd0930f19e43901
AnthonyAl <saviolidisalexandris@gmail.com>
2023-03-20 00:42:52 +0200

    Implementation for rho (alias rho_new) (!Subject to change) + fixed implementation for rho_positive (alias old rho)

--- src/core/Pre-Processor.py
+++ src/core/Pre-Processor.py
@@ -11,13 +11,16 @@ save_path_users = os.path.join(here, "../../dataset/users_test.csv")
 # loading the users dataframe
 users_df = pd.read_csv(os.path.join(here, "../../dataset/users.csv"))
 
-# the dataframe structure will be: u, items, ratings, rho, n_interactions
+# the final dataframe structure will be: u, items, ratings, rho, rho_positive, n_interactions
 l_rho = []
+l_rho_positive = []
 for l_ratings in users_df['ratings']:
     l_rho.append(utils.calc_rho(l_ratings))
+    l_rho_positive.append(utils.calc_rho_positive(l_ratings))
 
 users_df['rho'] = l_rho
-users_df.reindex(columns=['u', 'items', 'ratings', 'rho', 'n_interactions'])
+users_df['rho_positive'] = l_rho_positive
+users_df.reindex(columns=['u', 'items', 'ratings', 'rho', 'rho_positive', 'n_interactions'])
 print("\nCorrectly calculated and injected rho information to users dataframe.")
 print("Saving to file...")
 users_df.to_csv(save_path_users, index=False)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,7 @@ from src.utils import rhoCalculation
 
 def calc_rho(l_ratings):
     return rhoCalculation.calc_rho(l_ratings)
+
+
+def calc_rho_positive(l_ratings):
+    return rhoCalculation.calc_rho_positive(l_ratings)
