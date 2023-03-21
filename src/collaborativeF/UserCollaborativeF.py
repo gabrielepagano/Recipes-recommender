b4b102dea6792a05a60b78e58541cc2fade7dd3b
gabrielepagano <10578117@polimi.it>
2023-03-21 12:21:28 +0100

    Relevance scores list computation and ordering

--- src/collaborativeF/UserCollaborativeF.py
+++ src/collaborativeF/UserCollaborativeF.py
@@ -34,19 +34,19 @@ u_id = 12  # our reference user
 users_df = pd.read_csv("../../dataset/users.csv")
 recipes_df = pd.read_csv("../../dataset/recipes.csv")
 relevance_scores = []
-# TODO: Python sees items as a concatenation of chars and ratings as a unique string (does the same thing for u_items and u_ratings)
-uid_items = users_df.iloc[u_id]['items']  # ids of the items rated by u_id
-uid_ratings = users_df.iloc[u_id]['ratings']
+uid_items = utils.fromStringToIntList(users_df.iloc[u_id]['items'])  # ids of the items rated by u_id
+uid_ratings = utils.fromStringToFloatList(users_df.iloc[u_id]['ratings'])
 uid_ratings_full = np.zeros(len(recipes_df.index))  # ratings of uid, but also considering non-rated items
 for i in range(len(uid_items)):
     index = uid_items[i]
-    print(index)
     uid_ratings_full[index] = uid_ratings[i]
-for u in range(users_df.index):
+for u in range(len(users_df.index)):
+    print("Currently the user {} is being processed...".format(u))
+    couple = [u]
     if u != u_id:
         rho = users_df.iloc[u]['rho']
-        u_items = users_df.iloc[u]['items']  # ids of the items rated by u
-        u_ratings = users_df.iloc[u]['ratings']
+        u_items = utils.fromStringToIntList(users_df.iloc[u]['items'])  # ids of the items rated by u
+        u_ratings = utils.fromStringToFloatList(users_df.iloc[u]['ratings'])
         u_ratings_full = np.zeros(len(recipes_df.index))  # ratings of u, but also considering non-rated items
         for i in range(len(u_items)):
             index = u_items[i]
@@ -55,14 +55,21 @@ for u in range(users_df.index):
         n_good = 0.75 * n_items4 + n_items5  # we can eventually try different models and change that 0.75
         n_different = utils.itemsDifferenceCalculation(u_items, uid_items)
         utility = n_good / n_different
-        cos_similarity = np.dot(u_ratings_full, uid_ratings_full) / (norm(u_ratings_full) * norm(uid_ratings_full))
+        norm_prod = norm(u_ratings_full) * norm(uid_ratings_full)
+        if norm_prod == 0:
+            cos_similarity = 0.0
+        else:
+            cos_similarity = np.dot(u_ratings_full, uid_ratings_full) / norm_prod
         if cos_similarity == 1:
-            relevance_score = 0
+            relevance_score = 0.0
         else:
             relevance_score = cos_similarity * utility * rho
-        relevance_scores.append(relevance_score)
+        couple.append(relevance_score)
     else:
-        relevance_scores.append(1)  # A value that would be impossible to obtain. It's just a placeholder that we need
+        couple.append(1.0)  # A value that would be impossible to obtain. It's just a placeholder that we need
         # to understand that the current array cell we are considering in the relevance_scores
         # array is referred to the reference user itself.
-print(relevance_scores)
+    relevance_scores.append(couple)  # relevance_scores will be a list of lists containing both the relevance_score of
+    # that specific user and its user ID
+
+relevance_scores.sort(reverse=True, key=lambda x: x[1])  # reverse ordering by rating
